2f7e1e29a594ab3a2f97b84ff5185b4527e59663
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-09 16:19:20 +0200

    правки

--- task_3_vkluchite_svet/Form1.cs
+++ task_3_vkluchite_svet/Form1.cs
@@ -20,32 +20,6 @@ namespace task_3_vkluchite_svet
             if (e.KeyChar == (char)Keys.Enter)
             {
                 timer1.Start();
-                this.Location = new Point(0, 0);
-                this.Size = new Size(300, 300);
-                this.BackColor = Color.Red;
-                this.TopMost = true;
-
-                // Перемещение окна Form1 по периметру экрана, по часовой стрелке.
-                while (this.Location.X < Screen.PrimaryScreen.Bounds.Width - this.Width)
-                {
-                    this.Location = new Point(this.Location.X + 10, this.Location.Y);
-                    Thread.Sleep(1);
-                }
-                while (this.Location.Y < Screen.PrimaryScreen.Bounds.Height - this.Height)
-                {
-                    this.Location = new Point(this.Location.X, this.Location.Y + 10);
-                    Thread.Sleep(1);
-                }
-                while (this.Location.X > 0)
-                {
-                    this.Location = new Point(this.Location.X - 10, this.Location.Y);
-                    Thread.Sleep(1);
-                }
-                while (this.Location.Y > 0)
-                {
-                    this.Location = new Point(this.Location.X, this.Location.Y - 10);
-                    Thread.Sleep(1);
-                }
             }
             if (e.KeyChar == (char)Keys.Escape)
                 timer1.Stop();
@@ -54,6 +28,33 @@ namespace task_3_vkluchite_svet
         private void timer1_Tick(object sender, EventArgs e)
         {
             this.Text = DateTime.Now.ToString();
+
+            this.Location = new Point(0, 0);
+            this.Size = new Size(300, 300);
+            this.BackColor = Color.Red;
+            this.TopMost = true;
+
+            // Перемещение окна Form1 по периметру экрана, по часовой стрелке.
+            while (this.Location.X < Screen.PrimaryScreen.Bounds.Width - this.Width)
+            {
+                this.Location = new Point(this.Location.X + 5, this.Location.Y);
+                Thread.Sleep(1);
+            }
+            while (this.Location.Y < Screen.PrimaryScreen.Bounds.Height - this.Height)
+            {
+                this.Location = new Point(this.Location.X, this.Location.Y + 5);
+                Thread.Sleep(1);
+            }
+            while (this.Location.X > 0)
+            {
+                this.Location = new Point(this.Location.X - 5, this.Location.Y);
+                Thread.Sleep(1);
+            }
+            while (this.Location.Y > 0)
+            {
+                this.Location = new Point(this.Location.X, this.Location.Y - 5);
+                Thread.Sleep(1);
+            }
         }
     }
 }
